b384f2ec9a0feae23994518cf5622b8a7964901c
rjhallsted <rjhallsted@gmail.com>
2018-02-14 19:35:02 -0800

    Fleshing out flag structure and validation

--- inc/ft_ssl.h
+++ inc/ft_ssl.h
@@ -6,12 +6,27 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/12 16:25:27 by rhallste          #+#    #+#             */
-/*   Updated: 2018/02/12 22:25:12 by rhallste         ###   ########.fr       */
+/*   Updated: 2018/02/14 19:33:56 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SSL_H
 # define FT_SSL_H
+
+enum e_modes {
+	MODE_ERROR,
+	MODE_ENCRYPT,
+	MODE_DECRYPT
+};
+
+typedef struct	s_flag_data {
+	char		*command;
+	int			has_input_file;
+	char		*input_file;
+	int			has_output_file;
+	char		*output_file;
+	int			mode;
+}				t_flag_data;
 
 char *base64_encode(const char *input);
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/12 14:19:52 by rhallste          #+#    #+#             */
-/*   Updated: 2018/02/13 11:37:51 by rhallste         ###   ########.fr       */
+/*   Updated: 2018/02/14 19:34:47 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
  * DES accepts 8 (64 bits)
  */
 
-char *get_block(int fd, int block_size)
+char		*get_block(int fd, int block_size)
 {
 	char	buffer[block_size + 1];
 	int		ret;
@@ -36,12 +36,61 @@ char *get_block(int fd, int block_size)
 	return ((prog > 0) ? ft_strdup(buffer) : NULL);
 }
 
-int main(int argc, char **argv)
+void		flag_error(void)
 {
-	int		block_size;
-	char	*block;
-	char	*output;
-	int		fd;
+	ft_printf("Usage:\t./ft_ssl base64 [-ed] [-i in_file] [-o out_file]\n");
+	ft_printf("\t\t-e, --encode\tencodes input\n");	
+	ft_printf("\t\t-d, --decode\tdecodes input\n");
+	ft_printf("\t\t-i, --input\tinput file (default: \"-\" for stdin)\n");
+	ft_printf("\t\t-o, --output\toutput file (default: \"-\" for stdout)\n");
+}
+
+t_flag_data get_flags(int argc, char **argv)
+{
+	t_flag_data flag_data;
+	int			i;
+
+	flag_data.command = ft_strdup(argv[1]);
+	flag_data.mode = MODE_ENCRYPT;
+	flag_data.has_input_file = 0;
+	flag_data.input_file = NULL;
+	flag_data.has_output_file = 0;
+	flag_data.output_file = NULL;
+	i = 2;
+	while (i < argc)
+	{
+		if (ft_strcmp("-e", argv[i]) == 0 || ft_strcmp("--encrypt", argv[i]) == 0)
+			flag_data.mode = MODE_ENCRYPT;
+		else if (ft_strcmp("-d", argv[i]) == 0 || ft_strcmp("--decrypt", argv[i]) == 0)
+			flag_data.mode = MODE_DECRYPT;
+		else if (ft_strcmp("-i", argv[i]))
+		{
+			flag_data.has_input_file = 1;
+			flag_data.input_file = ft_strdup(argv[++i]);
+		}
+		else if (ft_strcmp("-o", argv[i]))
+		{
+			flag_data.has_output_file = 1;
+			flag_data.output_file = ft_strdup(argv[++i]);
+		}
+		else
+		{
+			flag_error();
+			flag_data.mode = MODE_ERROR;
+			return (flag_data);
+		}
+		i++;
+	}
+	return (flag_data);
+}
+
+int			main(int argc, char **argv)
+{
+	int			block_size;
+	char		*block;
+	char		*output;
+	int			fd;
+	t_flag_data	flag_data;
 	
 	if (argc < 2)
 	{
@@ -49,7 +98,10 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	fd = 0;
-	if (ft_strcmp(argv[1], "base64") == 0)
+	flag_data = get_flags(argc, argv);
+	if (flag_data.mode == MODE_ERROR)
+		return (-1);
+	if (ft_strcmp(flag_data.command, "base64") == 0)
 	{
 		block_size = 3;
 //		if ((fd = open(STDIN_FILENO, O_RDONLY)) == -1)
