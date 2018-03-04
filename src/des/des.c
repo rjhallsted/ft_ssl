6c7b79113955aec19d47a9366849b61515faa80d
rjhallsted <rjhallsted@gmail.com>
2018-03-03 19:30:33 -0800

    progress on encryption func

--- src/des/des.c
+++ src/des/des.c
@@ -6,15 +6,12 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/03 15:36:46 by rhallste          #+#    #+#             */
-/*   Updated: 2018/03/03 19:04:11 by rhallste         ###   ########.fr       */
+/*   Updated: 2018/03/03 19:30:00 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/libft/inc/libft.h"
 #include "../../inc/ft_ssl.h"
-
-extern const unsigned int expanPerm[48];
-extern const unsigned int pboxPerm[32];
 
 static void swap_ul(unsigned int *a, unsigned int *b)
 {
@@ -22,6 +19,9 @@ static void swap_ul(unsigned int *a, unsigned int *b)
 	*b ^= *a;
 	*a ^= *b;
 }
+
+extern const unsigned int expanPerm[48];
+extern const unsigned int pboxPerm[32];
 
 static void des_round(unsigned long *left, unsigned long *right,
 					  unsigned long *key, int round)
@@ -37,23 +37,30 @@ static void des_round(unsigned long *left, unsigned long *right,
 	swap_ul(left, right);
 }
 
-int	ftssl_des_encrypt(ftssl_args_t args, char *input, char *output, int len)
+extern const unsigned int initPerm[64];
+extern const unsigned int keyPerm[56];
+extern const unsigned int finalPerm[64];
+
+unsigned long	ftssl_des_encrypt(unsigned long key, unsigned long input);
 {
 	int i;
 	unsigned long left;
 	unsigned long right;
-	
-	//init permutation
-	//key permutation
-	//split to left and right
+
+	input = ftssl_des_permute(input, initPerm, 64);
+	key = ftssl_des_permute(key, keyPerm, 56);
+	left = (input >> 32) & 0xffffffff;
+	right = input & 0xffffffff;
 	i = 0;
 	while (i < 16)
-		des_round(&left, &right, args.keyval, i++);
-	//join left and right
-	//final permutation
+		des_round(&left, &right, key, i++);
+	input = left << 32;
+	input |= right;
+	input = ftssl_des_permute(input, finalPerm, 64);
+	return (input);
 }
 
-int	ftssl_des_decrypt(ftssl_args_t args, char *input, char *output, int len)
+unsigned long	ftssl_des_decrypt(unsigned long key, unsigned long input)
 {
 
 }
@@ -61,12 +68,19 @@ int	ftssl_des_decrypt(ftssl_args_t args, char *input, char *output, int len)
 int ftssl_des_ecb(ftssl_args_t args, const unsigned char *input,
 					char *output, int len)
 {
-	int		reslen;
+	int				reslen;
+	unsigned long	input_val;
+	unsigned long	output_val;
+	char			output_str[9];
 
+	input_val = (unsigned long)*input & 0xffffffffffffffff;
 	if (args.mode == FTSSL_MODE_DEC)
-		reslen = ftssl_des_decrypt(args, input, output, len);
+		output_val = ftssl_des_decrypt(args.key, input_val);
 	else
-		reslen = ftssl_des_encrypt(args, input, output, len);
+		output_val = ftssl_des_encrypt(args.key, input_val);
+	output_str = (char *)&output_val;
+	output_str[8] = '\0';
+	ft_strncpy(output, output_str, 9);
 	return (reslen);
 }
 
